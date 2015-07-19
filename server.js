e719ae692d3c2e42ed7247aeac5bced4cd600bbb
sjacob95 <sjacob95@vt.edu>
2015-07-19 12:29:50 -0400

    Jade is working... And I'm using a more comfortable setup

---
+++ server.js
@@ -0,0 +1,35 @@
+var express         = require('express');
+var app             = express();
+var stylus          = require('stylus');
+var nib             = require('nib');
+var mongoose        = require('mongoose');
+var bodyParser      = require('body-parser');
+
+var app = express();
+//var db = mongoose.connect('mongodb://localhost/contactlist');
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
+app.set('views', __dirname + '/app');
+app.set('view engine', 'jade');
+/**app.use(express.logger('dev'));
+app.use(stylus.middleware(
+  { src: __dirname + '/app'
+  , compile: compile
+  }
+));**/
+app.use(express.static(__dirname + "/app"));
+app.use(bodyParser.json());
+
+
+app.get('/heyyo', function (req, res)    {
+    res.render('tester');
+    console.log('Heyyoooooooooooo World!');
+})
+
+app.listen(3000);
+console.log("Server running on port 3000");
