d204411db8161d73233b0de2a97e995682e3e3c3
sjacob95 <sjacob95@vt.edu>
2015-08-02 00:41:51 -0400

    Swagged out logo animation and navbar

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function(config){
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'app/bower_components/angular-animate/angular-animate.js'
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
       'app/view*/**/*.js'
